1550c3b05b776b9c6858314a7edd1d940c17bbd5
Fillah Alamsyah <87614211+FillahAlamsyah@users.noreply.github.com>
2022-12-27 19:13:27 +0700

    Update

--- PrediksiPosisiGerakParabola.py
+++ PrediksiPosisiGerakParabola.py
@@ -7,20 +7,20 @@ import pandas as pd
 
 #Database: Gerbang LOgika AND
 #Membaca data dari file
-FileDB = 'DatabaseIntegralTrapezoid.txt'
+FileDB = 'DatabaseGerakParabola.txt'
 Database = pd.read_csv(FileDB, sep=",", header=0)
 
 #x = Data, y=Target
-X = Database[[u'a', u'b']]
-y = Database.Target
+X = Database[[u't']]
+y = [Database.Posisiy, Database.Posisix]
 
 
 clf = svm.SVC()
 clf.fit(X.values,y)
 
-print(clf.predict( [[1,9]] ))
-print(clf.predict( [[1,2]] ))
-print(clf.predict( [[2,5]] ))
-print(clf.predict( [[3,4]] ))
-print(clf.predict( [[9,6]] ))
+print(clf.predict( [[0.2]] ))
+print(clf.predict( [[0.3]] ))
+print(clf.predict( [[1.5]] ))
+print(clf.predict( [[2.2]] ))
+print(clf.predict( [[2.5]] ))
 
